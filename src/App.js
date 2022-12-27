5829050d93f5e6013624866904644081914897ca
CBB1017 <gtu1017@gmail.com>
2022-12-27 11:08:55 +0900

    Todo List Add

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
 import {useEffect, useState} from "react";
 
+function Hello() {
+    useEffect(() => {
+        console.log("cre ated");
+    }, [])
+    return <h1>Hello</h1>
+}
 function App() {
-    const [counter, setValue] = useState(0);
-    const [keyword, setKeyword] = useState("");
-    const onClick = () => setValue((prev) => prev + 1);
-    const onChange = (event) => setKeyword(event.target.value);
-    const iRunOnlyOnce = () => {
-        console.log('i run all the time')
+    const [toDo, setToDo] = useState("");
+    const [toDos, setToDos] = useState([]);
+    const onChange = (event) => setToDo(event.target.value);
+    const onSubmit = (event) => {
+        event.preventDefault();
+        if( toDo === ""){
+            return;
+        }
+        setToDos((currentArray) => [toDo, ...currentArray]);
+        setToDo("");
     }
-    useEffect(iRunOnlyOnce, []);
 	return (
 		<div>
-            <input
-               value={keyword}
-               onChange={onChange}
-               type="text"
-               placeholder="Search here..."
-            />
-			<h1>{counter}</h1>
-            <button onClick={onClick}>click me</button>
+            <h1>My To Dos ({toDos.length})</h1>
+            <form onSubmit={onSubmit}>
+                <input
+                    onChange={onChange}
+                    value={toDo}
+                    type="text"
+                    placeholder="Write your to do..."
+                />
+                <button>Add To Do</button>
+            </form>
+            <hr />
+            <ul>
+                {toDos.map((item, index) => (
+                    <li key={index}>{item}</li>
+                ))}
+            </ul>
 		</div>
 	);
 }
